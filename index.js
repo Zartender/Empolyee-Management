8925434ece98281a13e0daa9fb5c1d865019a068
Ian Greenblott <imgreenblott@gmail.com>
2021-12-07 19:09:47 -0500

    Madded kvp port to const db, added methods, functions and kvp's to make the app run effectively.

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const mysql = require('mysql2');
 
 require('console.table');
 require('dotenv').config();
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: 'localhost',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-});
+  port: 3306
+},
+console.log(`Connected to db`)
+);
 
-const db = connection.connect(function (err) {
-  if (err) throw err;
-  console.log('Connected to db successfully');
-});
+db.connect(err => {
+  if(err) throw err;
+  console.log('connected');
+  init()
+})
+
 
 function init() {
   console.log("Welcome to Employee Tracker!");
@@ -35,18 +40,114 @@ function init() {
       console.log(answers)
 
       // connection.query('SELECT * from employee', )
-    })
-    .catch((error) => {
-      if (error) {
-        // Prompt couldn't be rendered in the current environment
-        console.error(error);
-      } else {
-        // Something else went wrong
-      }
-    });
-  //Question 
-
+      if(answers.init === 'Add Employee') {
+        addEmployee();
+    } else if (answers.init === 'View All Departments') {
+        viewDept();
+    } else if (answers.init === 'Add Department') {
+        addDept();
+    } else if (answers.init === 'Add Role') {
+        addRole();
+    } else if (answers.init === 'View All Employees') {
+        viewEmployees();
+    } else if (answers.init === 'View All Roles') {
+        viewRoles();
+    } else {
+        finish();
+    }
+})
 
 }
+
+
+    
+function addEmployee() {
+  inquirer.prompt([
+  {
+      type: 'input',
+      name: 'firstName',
+      message: 'What is the employees first name?',
+  },
+  {
+      type: 'input',
+      name: 'lastName',
+      message: 'What is the employees last name?',    
+  },
+  {
+      type: 'input',
+      name: 'role',
+      message: 'What is the employees role?',  
+  },
+  {
+      type: 'input',
+      name: 'manager',
+      message: 'Who is the employees manager?', 
+  },
+  ])
+  .then((answers) => {
+      // console.log(answers);
+      let query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answers.firstName}', '${answers.lastName}', '${answers.role}', '${answers.manager}');`;
+      db.promise().query(query).then(function(err, res){
+          if(err){
+              // console.error(err);
+          }
+          // console.log(res);
+          console.log('Employee Added Successfully');
+          menu();
+      });
+
+  });
+}
+
+function addRole() {
+  inquirer.prompt([
+  {
+      type: 'input',
+      name: 'newRole',
+      message: 'What new role would you like to add?',
+  },
+  {
+      type: 'input',
+      name: 'salary',
+      message: 'What is the salary for this role?',
+  },
+  {
+      type: 'input',
+      name: 'department',
+      message: 'What department does this role belong to?',
+  },
+  ])
+  .then((answers) => {
+      let query = `INSERT INTO roles (title, salary, department_id) VALUES ('${answers.newRole}', ${answers.salary}, ${answers.department})`;
+      db.promise().query(query).then(function(err, res){
+         if(err){
+             // console.error(err);
+         }
+         // console.log(res);
+         console.log('Successful Addition');
+         menu();
+     });
+  }) 
+}
+
+function viewEmployees() {
+  db.query('SELECT * FROM employee', (err, data) => {
+      if(err) throw err;
+      console.table(data);
+      menu();
+});
+}
+
+function viewRoles() {
+ db.query('SELECT * FROM roles', (err, data) => {
+     if(err) throw err;
+     console.table(data);
+     menu();
+});
+}
+
+function finish() {
+ console.log('Fin')
+};
 
 init();
